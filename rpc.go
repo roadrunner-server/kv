30be5fe485f534feb33db7fcbebd9d1a7644d62c
Valery Piashchynski <piashchynski.valery@gmail.com>
2023-08-10 14:38:07 +0200

    chore: linters
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- rpc.go
+++ rpc.go
@@ -1,9 +1,9 @@
 package kv
 
 import (
+	kvv1 "github.com/roadrunner-server/api/v4/build/kv/v1"
 	"github.com/roadrunner-server/api/v4/plugins/v1/kv"
 	"github.com/roadrunner-server/errors"
-	kvv1 "github.com/roadrunner-server/api/v4/build/kv/v1"
 )
 
 // Wrapper for the plugin
